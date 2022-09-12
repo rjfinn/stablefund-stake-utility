f2c1de6920a8feac06aedb5b1ab47dcd3327bef9
Richard Finn <rf7931@att.com>
2022-09-12 15:30:57 -0500

    fixed console.log typo

--- src/StakeUtil.js
+++ src/StakeUtil.js
@@ -391,7 +391,7 @@ export default function StakeUtil(params) {
                     eligible += element.amount;
                 }
             });
-            console.log(`Number of depoists, active/total: ${deposits.length}/${depositCounts[value.address]}`);
+            console.log(`Number of deposits, active/total: ${deposits.length}/${depositCounts[value.address]}`);
             console.log('Capital:', Number(capital.toFixed(2)),
                 `(\$${(capital * price).toFixed(2)})`);
             console.log('Withdraw eligible:', Number(eligible.toFixed(2)),
